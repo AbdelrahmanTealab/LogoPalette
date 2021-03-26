c87db2b6c0bbaced8e6e06a8299513ed1d328154
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2021-03-26 13:10:36 -0400

    implement Vision and CoreML base functionalities

--- LogoPalette/ViewController.swift
+++ LogoPalette/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var photoDisplay: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
     }
+    
 
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image:CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("loading CoreML model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("error reading the request results")
+            }
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+}
 
+extension ViewController: UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userEditedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            photoDisplay.image = userEditedImage
+            guard let ciimage = CIImage(image: userEditedImage) else {
+                fatalError("Error while converting image to ciimage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
